fd2825bdf5b791f15861ca1a05c479825b17d255
MEREDITH\binugant <binugant@meredith.com>
2019-04-21 18:05:48 -0700

    update 4/21

--- scripts/deploy_sg.sh
+++ scripts/deploy_sg.sh
@@ -12,16 +12,17 @@ TEMPLATE=cfn/template.yml
 CUSTOMER=mdcp
 PURPOSE=hosting
 
-cfn-lint --ignore-checks W2001 W8001 -t $TEMPLATE
-yamllint -d "{extends: relaxed, rules: {line-length: {max: 150}}}" $TEMPLATE
+# cfn-lint --ignore-checks W2001 W8001 -t $TEMPLATE
+# yamllint -d "{extends: relaxed, rules: {line-length: {max: 150}}}" $TEMPLATE
 
-docker-compose run digops-stacks digops-stacks change-set $STACK_NAME $TEMPLATE \
-	       --environment $ENV \
-	       --region $REGION \
-	       --customer $CUSTOMER \
-	       --purpose $PURPOSE
+aws cloudformation deploy --template-file ./cfn/template.yml --stack-name test-stack-abc98383838
+# docker-compose run digops-stacks digops-stacks change-set $STACK_NAME $TEMPLATE \
+	       # --environment $ENV \
+	       # --region $REGION \
+	       # --customer $CUSTOMER \
+	       # --purpose $PURPOSE
 
-docker-compose run digops-stacks digops-stacks execute $STACK_NAME \
-	       --environment $ENV \
-	       --region $REGION
+# docker-compose run digops-stacks digops-stacks execute $STACK_NAME \
+	       # --environment $ENV \
+	       # --region $REGION
 
