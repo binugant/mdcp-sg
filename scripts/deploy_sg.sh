31c34b96eb17a4f894e7a999cb7d5a74a2ef3118
MEREDITH\binugant <binugant@meredith.com>
2019-04-21 18:35:20 -0700

    4/21 2nd

--- scripts/deploy_sg.sh
+++ scripts/deploy_sg.sh
@@ -5,17 +5,17 @@ if [ -z "$1" ];  then
    exit 1
 fi
 
-ENV=environment/$1
-REGION=us-east-1
-STACK_NAME=onesg-aws034876-cfn
-TEMPLATE=cfn/template.yml
-CUSTOMER=mdcp
-PURPOSE=hosting
+# ENV=environment/$1
+# REGION=us-east-1
+# STACK_NAME=onesg-aws034876-cfn
+# TEMPLATE=cfn/template.yml
+# CUSTOMER=mdcp
+# PURPOSE=hosting
 
 # cfn-lint --ignore-checks W2001 W8001 -t $TEMPLATE
 # yamllint -d "{extends: relaxed, rules: {line-length: {max: 150}}}" $TEMPLATE
 
-aws cloudformation deploy --template-file ./cfn/template.yml --stack-name test-stack-abc98383838
+aws cloudformation deploy --template-file ./cfn/template.yml --stack-name test-stack-abc98383838 --region us-east-1 
 # docker-compose run digops-stacks digops-stacks change-set $STACK_NAME $TEMPLATE \
 	       # --environment $ENV \
 	       # --region $REGION \
