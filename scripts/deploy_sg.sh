556ea5dc0dde0342781a45b0b18d2b05bf52ee53
MEREDITH\binugant <binugant@meredith.com>
2019-04-28 19:05:08 -0700

    updated

--- scripts/deploy_sg.sh
+++ scripts/deploy_sg.sh
@@ -5,24 +5,24 @@ if [ -z "$1" ];  then
    exit 1
 fi
 
-# ENV=environment/$1
-# REGION=us-east-1
-# STACK_NAME=onesg-aws034876-cfn
-# TEMPLATE=cfn/template.yml
-# CUSTOMER=mdcp
-# PURPOSE=hosting
+ENV=environment/$1
+REGION=us-east-1
+STACK_NAME=onesg-aws034876-cfn
+TEMPLATE=cfn/template.yml
+CUSTOMER=mdcp
+PURPOSE=hosting
 
-# cfn-lint --ignore-checks W2001 W8001 -t $TEMPLATE
-# yamllint -d "{extends: relaxed, rules: {line-length: {max: 150}}}" $TEMPLATE
+cfn-lint --ignore-checks W2001 W8001 -t $TEMPLATE
+yamllint -d "{extends: relaxed, rules: {line-length: {max: 150}}}" $TEMPLATE
 
-aws cloudformation deploy --template-file ./cfn/template.yml --stack-name test-stack-abc98383838 --region us-east-1 --environment prod --region us-east-1
-# docker-compose run digops-stacks digops-stacks change-set $STACK_NAME $TEMPLATE \
-	       # --environment $ENV \
-	       # --region $REGION \
-	       # --customer $CUSTOMER \
-	       # --purpose $PURPOSE
+#aws cloudformation deploy --template-file ./cfn/template.yml --stack-name test-stack-abc98383838 --region us-east-1 --environment prod --region us-east-1
+  docker-compose run digops-stacks digops-stacks change-set $STACK_NAME $TEMPLATE \
+	       --environment $ENV \
+	       --region $REGION \
+	       --customer $CUSTOMER \
+	       --purpose $PURPOSE
 
-# docker-compose run digops-stacks digops-stacks execute $STACK_NAME \
-	       # --environment $ENV \
-	       # --region $REGION
+  docker-compose run digops-stacks digops-stacks execute $STACK_NAME \
+	       --environment $ENV \
+	       --region $REGION
 
