fcb5ad7b9b1362dd0e99d5ec30a2711cf097aa1a
MEREDITH\binugant <binugant@meredith.com>
2019-04-30 14:03:22 -0700

    almost near

--- scripts/deploy_sg.sh
+++ scripts/deploy_sg.sh
@@ -17,13 +17,13 @@ else
 fi
 
 REGION=us-east-1
-STACK_NAME=parenting-wordpress
-BASE_STACK_NAME="${STACK_NAME}-Base"
+STACK_NAME=StackSet-sg-bhaskar
+#BASE_STACK_NAME="${STACK_NAME}-security"
 TEMPLATE=cfn/template.yml
-BASE_TEMPLATE=cfn/template.yml
-CUSTOMER=corecms
+#BASE_TEMPLATE=cfn/template.yml
+CUSTOMER=bhaskar
 PURPOSE=hosting
-NESTED_STACK_VERSION='version/13.9.4'
+#NESTED_STACK_VERSION='version/13.9.4'
 
 
 if ! $(command -v yamllint >/dev/null 2>&1); then
@@ -31,32 +31,32 @@ if ! $(command -v yamllint >/dev/null 2>&1); then
 fi
 
 
-# create beanstalk application on master only
-if [[ $CIRCLE_BRANCH == "master" ]]; then
+# # create beanstalk application on master only
+# if [[ $CIRCLE_BRANCH == "master" ]]; then
     
-    echo "On master branch. Creating base resources"
-    set -exo pipefail
-    echo
-    cfn-lint --ignore-checks W1020 W2001 W8001 -t $TEMPLATE
-    yamllint -d "{extends: relaxed, rules: {line-length: {max: 150}}}" $TEMPLATE
-    $DOCKER_CMD_PREFIX digops-stacks change-set $BASE_STACK_NAME $BASE_TEMPLATE \
-    	--environment production \
-        --region $REGION \
-	--customer $CUSTOMER \
-	--purpose $PURPOSE
+    # echo "On master branch. Creating base resources"
+    # set -exo pipefail
+    # echo
+    # cfn-lint --ignore-checks W1020 W2001 W8001 -t $TEMPLATE
+    # yamllint -d "{extends: relaxed, rules: {line-length: {max: 150}}}" $TEMPLATE
+    # $DOCKER_CMD_PREFIX digops-stacks change-set $BASE_STACK_NAME $BASE_TEMPLATE \
+    	# --environment production \
+        # --region $REGION \
+	# --customer $CUSTOMER \
+	# --purpose $PURPOSE
 
-    $DOCKER_CMD_PREFIX digops-stacks execute $BASE_STACK_NAME \
-    	--environment production \
-	--region $REGION
+    # $DOCKER_CMD_PREFIX digops-stacks execute $BASE_STACK_NAME \
+    	# --environment production \
+	# --region $REGION
 
-    exit 0
-fi
+    # exit 0
+# fi
 
 
 regex="^environment\/([0-9a-z]*)"
 if [[ $CIRCLE_BRANCH =~ $regex ]]; then
     env="${BASH_REMATCH[1]}"
-    echo "Will attempt beanstalk $env environment deployment."
+    echo "Will attempt sg $env environment deployment."
 else
     echo "Failed regex match '$regex'. Exiting."
     exit 1
